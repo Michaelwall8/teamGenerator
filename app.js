a69f411d68e9761261efedb181553081708fbd23
Michael Wall <mikesterwallace8@gmail.com>
2021-02-01 23:55:27 -0500

    Finalized readMe file and function to generate app

--- app.js
+++ app.js
@@ -21,4 +21,156 @@ const writeFileP = util.promisify(fs.writeFile)
 // Render html
 const render = require("./lib/htmlRenderer");
 
+// --------------------------------------------------------------\ Program /-----------------------------------------------------------------
 
+let teamMembers = [];
+console.log(`
+              \x1b[95mWelcome to Team GENERATOR!\x1b[39m
+     \x1b[36mEnter the information of each member correctly.
+    The app will stop after you enter the last member.\x1b[89m
+                 \x1b[96m>>> Get started <<<\x1b[39m
+    `)
+console.log(`\n\x1b[93m>>>   Team Member   <<<\x1b[39m`)
+generateApp()
+
+
+// Functions
+
+function generateApp() {    
+
+    inquirer
+        .prompt([{
+            message: 'Enter the Name',
+            name: 'name'
+            },
+            {
+            type: 'list',
+            message: 'Select the Role',
+            choices: ['Manager', 'Engineer', 'Intern'],
+            name: 'role'
+            },
+            {
+            message: 'Enter the ID',
+            name: 'id'
+            },
+            {
+            message: 'Enter the email',
+            name: 'email'
+            }
+        ])
+        .then(function ({ name, role, id, email }) {
+            if (role === "Manager") {
+                inquirer
+                    .prompt([{
+                        message: 'Enter office number',
+                        name: 'officeNumber'
+                        },
+                        {
+                        type: 'list',
+                        message: 'Would you like to add an other member to your team?',
+                        choices: ['Yes', 'No'],
+                        name: 'newMember'
+                        }
+                    ])
+                    .then(function({ officeNumber, newMember }){
+                        teamMembers.push(new Manager(name, id, email, officeNumber))
+                        
+                        
+                        if (newMember === 'Yes') {
+                            console.log(`\n\x1b[93m>>> New Team Member <<<\x1b[39m`)
+                            generateApp()
+                        }
+                        else {
+                            writeFileP(outputPath, render(teamMembers),function (err) {
+                                if (err) {
+                                  throw err
+                                }
+                                
+                                
+                            })
+                            console.log(`\x1b[92m\nSucced!, team.html file was created\x1b[39m`)
+                        }
+                        
+                    })
+            }
+            else if (role === "Engineer"){
+                inquirer
+                    .prompt([{
+                        message: 'Enter Github username',
+                        name: 'github'
+                        },
+                        {
+                        type: 'list',
+                        message: 'Would you like to add an other member to your team?',
+                        choices: ['Yes', 'No'],
+                        name: 'newMember'
+                        }
+                    ])
+                    .then(function({ github, newMember }){
+                        teamMembers.push(new Engineer(name, id, email, github))
+                        
+                        if (newMember === 'Yes') {
+                            console.log(`\n\x1b[93m>>> New Team Member <<<\x1b[39m`)
+                            generateApp()
+                        }
+                        else {
+                            writeFileP(outputPath, render(teamMembers),function (err) {
+                                if (err) {
+                                  throw err
+                                }
+                                
+                                
+                            })
+                            console.log(`\x1b[92m\nSucced!, team.html file was created\x1b[39m`)
+                        }
+                       
+                    })
+            }
+            else if (role === "Intern"){
+                inquirer
+                .prompt([{
+                    message: 'Enter The name of your school',
+                    name: 'school'
+                    },
+                    {
+                    type: 'list',
+                    message: 'Would you like to add an other member to your team?',
+                    choices: ['Yes', 'No'],
+                    name: 'newMember'
+                    }
+                ])
+                .then(function({ school, newMember }){
+                    teamMembers.push(new Intern(name, id, email, school))
+                  
+                    if (newMember === 'Yes') {
+                        console.log(`\n\x1b[93m>>> New Team Member <<<\x1b[39m`)
+                        generateApp()
+                    }
+                    else {
+                        writeFileP(outputPath, render(teamMembers),function (err) {
+                            if (err) {
+                              throw err
+                            }
+                            
+                            
+                        })
+                        console.log(`\x1b[92m\nSucced!, team.html file was created\x1b[39m`)
+                    }
+                })
+            }
+            else {
+                console.log('Wrong role, try again!')
+            }
+
+          
+        })
+        .catch(function (err) {
+            console.error(err)
+        })
+        
+
+}
+
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------\  END  /---------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------------------------
---
+++ newReadme.md
@@ -0,0 +1,44 @@
+# Team Generator
+   https://www.youtube.com/watch?v=49pHKRi9p_s&feature=youtu.be
+  ## Table of Content
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Test](#test)
+  * [Contributing](#contributing)
+  * [Badges](#badges)
+  * [License](#license)
+  * [Questions](#questions)
+
+  ## Description
+   Team generator is an app that produces pages with information pertaining individual team members. This app is ran through command lines in terminal an is powered by javascript, npm and data from the github API. 
+
+  ## Installation
+   1. Install node.js 
+   2. Clone the repository
+
+  ## Usage
+   Answer all the prompt questions
+
+  ## Test
+  None
+
+  ## Badges
+  ![Mike](https://img.shields.io/badge/version-v1.0.0-yellow) 
+  ![code size](https://img.shields.io/github/languages/code-size/Michaelwall8/teamGenerator)
+  ![top lang](https://img.shields.io/github/languages/top/Michaelwall8/teamGenerator)
+  ![last comit](https://img.shields.io/github/last-commit/Michaelwall8/teamGenerator)
+  ![vulnera](https://img.shields.io/snyk/vulnerabilities/github/Michaelwall8/teamGenerator)
+
+  ## License ![License](https://img.shields.io/github/license/Michaelwall8/teamGenerator)
+  This project is under the MIT License.
+  
+  ## Contributing [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)
+  This project is under the Contributor Covenant.
+
+  ## Questions ![Social](https://img.shields.io/github/followers/Michaelwall8?style=social) 
+  ![Profile img](https://avatars.githubusercontent.com/u/73609296?v=4)
+
+  E-mail: Hidden
+  
